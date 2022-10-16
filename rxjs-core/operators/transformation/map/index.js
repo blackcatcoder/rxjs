b1fc811865a38d0a7b269e8947eb6d33b08397f8
blackcat <vinv.2491@hotmail.com>
2022-10-16 17:54:34 +0700

    update

--- rxjs-core/operators/transformation/map/index.js
+++ rxjs-core/operators/transformation/map/index.js
@@ -1,1 +1,30 @@
-const { fromEvent, exhaustMap, interval, take, map } = rxjs;
+const { fromEvent, exhaustMap, interval, take, map, of, from } = rxjs;
+
+const students = [
+    {id: 1, name: 'anhvi 1'},
+    {id: 2, name: 'anhvi 2'},
+    {id: 3, name: 'anhvi 3'},
+    {id: 4, name: 'anhvi 4'},
+    {id: 5, name: 'anhvi 5'},
+];
+
+const observer = {
+    next: (data) => console.log(data)
+}
+
+// rxjs from
+const studentsFrom$ = from(students);
+const subscriptionFrom = studentsFrom$.pipe(map(student => student.name)).subscribe(observer);
+
+// rxjs of
+const studentsOf$ = of(students);
+const subscriptionOf = studentsOf$.pipe(map(students => students // this map belong rxjs
+    .map(student => student.name) // this map belong javascript
+    .filter(name => name === 'anhvi 2' || name === 'anhvi 3'))) // this filter belong javascript
+.subscribe(observer);
+
+
+setTimeout(() => {
+    subscriptionFrom.unsubscribe();
+    subscriptionOf.unsubscribe();
+}, 5000);
