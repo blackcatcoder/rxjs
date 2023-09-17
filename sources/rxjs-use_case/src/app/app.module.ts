106e7ba7791cc7193ae41cd279ae871a01af1378
blackcat <vinv.2491@hotmail.com>
2023-09-17 22:12:07 +0700

    update

--- sources/rxjs-use_case/src/app/app.module.ts
+++ sources/rxjs-use_case/src/app/app.module.ts
@@ -9,14 +9,6 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
 import { FormsModule } from '@angular/forms';
 import { Sample1Component } from './sample1/sample1.component';
 import { Sample2Component } from './sample2/sample2.component';
-import { Sample3Component } from './sample3/sample3.component';
-import { Sample4Component } from './sample4/sample4.component';
-import { Sample5Component } from './sample5/sample5.component';
-import { Sample6Component } from './sample6/sample6.component';
-import { Sample7Component } from './sample7/sample7.component';
-import { Sample8Component } from './sample8/sample8.component';
-import { Sample9Component } from './sample9/sample9.component';
-import { Sample10Component } from './sample10/sample10.component';
 import { Product2Service } from './sample2/service/Product2Service';
 import { Product1Service } from './sample1/service/User1Service';
 
@@ -27,14 +19,6 @@ import { Product1Service } from './sample1/service/User1Service';
     ProductDetailComponent,
     Sample1Component,
     Sample2Component,
-    Sample3Component,
-    Sample4Component,
-    Sample5Component,
-    Sample6Component,
-    Sample7Component,
-    Sample8Component,
-    Sample9Component,
-    Sample10Component
   ],
   imports: [
     BrowserModule,
